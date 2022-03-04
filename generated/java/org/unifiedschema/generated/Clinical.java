ec4ed431bbf5c74869dff5ffdd7edf5aae03b2a7
K. Joeri van der Velde <joeriv@gmail.com>
2022-03-04 14:18:58 +0100

    Update generated outputs

---
+++ generated/java/org/unifiedschema/generated/Clinical.java
@@ -0,0 +1,31 @@
+package org.unifiedschema.generated;
+
+import java.util.List;
+
+public class Clinical {
+	public String identifier;
+	public String belongstosubject;
+	public boolean affectedstatus;
+	public String dateofdiagnosis;
+	public int ageatdiagnosis;
+	public int ageofonset;
+	public int ageatlastscreening;
+	public List<String> observedphenotype;
+	public List<String> unobservedphenotype;
+	public List<String> provisionalphenotype;
+	public List<String> clinicaldiagnosis;
+	public List<String> moleculardiagnosisgene;
+	public String moleculardiagnosisother;
+	public List<String> medication;
+	public String drugregimen;
+	public List<String> medicalhistory;
+	public List<String> phenotypicdataavailable;
+	public String statusofdiagnosis;
+	public String firstcontact;
+	public String datediagnosisconfirmed;
+	public List<String> belongstodatarelease;
+	public List<String> familymembersaffected;
+	public List<String> familymemberssequenced;
+	public String functioning;
+	public String materialusedindiagnosis;
+}
