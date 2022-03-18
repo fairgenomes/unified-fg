4a149ecf57ae39978d88f4f342ac25bfd3762a7c
K. Joeri van der Velde <joeriv@gmail.com>
2022-03-18 13:27:13 +0100

    Update generated content

---
+++ generated/java/org/unifiedschema/generated/Ngsanalysis.java
@@ -0,0 +1,17 @@
+package org.unifiedschema.generated;
+
+import java.util.List;
+
+public class Ngsanalysis {
+	public String identifier;
+	public String belongstosequencing;
+	public String physicaldatalocation;
+	public String abstractdatalocation;
+	public List<String> dataformatsstored;
+	public String algorithmsused;
+	public String referencegenomeused;
+	public String bioinformaticprotocolused;
+	public String bioinformaticprotocoldeviation;
+	public String reasonforbioinformaticprotocoldeviation;
+	public String wgsguidelinefollowed;
+}
